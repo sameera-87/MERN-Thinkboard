8d64cda561b9e58b9ed3db3879128d76d9ddc844
sameera-87 <sameekumarasinghe87@gmail.com>
2025-10-18 23:26:49 +0530

    Home page created

--- frontend/frontend/src/components/Navbar.jsx
+++ frontend/frontend/src/components/Navbar.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router";
+import { PlusIcon } from "lucide-react"
+
 const Navbar = () => {
     return <header className="bg-base-300 border-b border-base-content/10">
         <div className="mx-auto max-w-6xl p-4">
@@ -5,7 +8,12 @@ const Navbar = () => {
                 <h1 className="text-3xl font-bold text-primary font-mono tracking-tighter">
                     ThinkBoard
                 </h1>
-                <div></div>
+                <div className="flex items-center gap-4">
+                    <Link to={"/create"} className="btn btn-primary">
+                    <PlusIcon className="size-5"/>
+                    <span>New Note</span>
+                    </Link>
+                </div>
             </div>
         </div>
     </header>
--- frontend/frontend/src/pages/HomePage.jsx
+++ frontend/frontend/src/pages/HomePage.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import { useState, useEffect } from "react";
+import RateLimitedUI from "../components/RateLimitedUI"
+import axios from "axios"
 
 function HomePage() {
+  const [isRateLimited, setRateLimited] = useState(true);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try{
+        const res = await axios.get("http://localhost:5001/api/notes")
+        const data = res.data;
+        console.log(data);
+      }
+      catch (error) {
+        console.log("Error fetching notes")
+      }
+    }
+
+    fetchNotes();
+  }, [])
+
   return (
     <div className='min-h-screen'>
       <Navbar />
-    </div>
-  )
-}
 
-export default HomePage
+      {isRateLimited && <RateLimitedUI />}
+    </div>
+  );
+};
+
+export default HomePage;
